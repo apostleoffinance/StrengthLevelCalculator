da9daafd9947c148e4dc419939c574c84ce10cef
Olaoluwa Tunmise <oluwatunmise1olaoluwa@gmail.com>
2025-04-16 15:04:21 +0100

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input your Body Weight (BW)");
+        double BW = sc.nextInt();
+
+        System.out.println("Input your Number of Squat (Sq)");
+        double Sq = sc.nextInt();
+
+        System.out.println("Input your Number of Bench Press (BP)");
+        double BP = sc.nextInt();
+
+        System.out.println("Input your Number of Deadlift (Dl)");
+        double Dl = sc.nextInt();
 
 
 
